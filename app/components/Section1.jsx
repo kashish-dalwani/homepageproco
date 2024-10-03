4698be8120ac3d167b995fcf42c01ebc9c80b563
kashish-dalwani <dalwanikashish25@gmail.com>
2024-10-03 20:22:11 +0530

    section1

---
+++ app/components/Section1.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Section1() {
+  return (
+    <div>
+      <div className='bg-[url(/images/background.png)] bg-cover bg-center h-screen mt-[-30px]'></div>
+<div className='mt-[-600px] text-center'>
+  <p className='text-6xl font-bold p-4'>
+    Where Visionary <br />Technology Drives <span className='text-[#8952FD]'>Excellence</span></p>
+  <p>At ProCohat, we transcend the conventional, forging new paths in digital innovation. As a<br></br> pioneer in technology solutions, we merge avant-garde technology with unrivaled industry<br></br> insight to deliver transformative services that surpass your highest expectations</p>
+  <img src='/images/centerimg.png' className='ml-[30rem] mt-10'></img>
+  <img src='/images/graph.png' className='mt-[-260px] ml-64'></img>
+  <img src='/images/code.png' className='ml-[47rem] mt-[-450px]'></img>
+  <img src='/images/arrowdown.png' className='ml-96 mt-[-250px]'></img>
+  <img src='/images/arrowup.png' className='ml-[54rem] mt-44'></img>
+  <img src='/images/proco.png' className='ml-[40rem] mt-[-130px]'></img>
+</div>
+<div className='flex space-x-6 text-3xl ml-32 font-bold text-center mt-56'>
+<p><p className='text-9xl text-[#E1E1E1]' style={{fontStyle:"italic"}}>150+</p>Projects</p>
+  <img src='/images/line.png'></img>
+  <p><p className='text-9xl text-[#E1E1E1]' style={{fontStyle:"italic"}}>100+</p>Clients</p>
+  <img src='/images/line.png'></img>
+  <p><p className='text-9xl text-[#E1E1E1]' style={{fontStyle:"italic"}}>10+</p>Services</p>
+</div>
+
+
+    </div>
+  )
+}
+
+export default Section1
